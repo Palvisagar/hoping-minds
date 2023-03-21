305c2b708522f00f93e0d4413c10045d3c9cecb5
palvisagar <pallvisagar.code@gmail.com>
2023-03-21 16:35:53 +0530

    javascript

---
+++ JS/21-Mar-2023/main.js
@@ -0,0 +1,31 @@
+// let a=32
+// let b=null
+// let c=true
+// let d=false
+// let e=BigInt(222)
+// let f="Palvi"
+// let g='palvi'
+// let h=Symbol("sagar")
+// let i=undefined
+
+// console.log(a,b,c,d,e,f,g,h,i)
+
+//  identify the type of var
+
+// let j=1234567
+// console.log(typeof j)
+
+var a = 9
+{
+    var a ="hiiiii"
+    console.log(a)
+}
+console.log(a)
+
+let b = 9
+{
+    var a ="hey"
+    console.log(a)
+}
+console.log(b)
+
